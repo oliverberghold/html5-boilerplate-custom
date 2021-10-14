dee2b3f92f1f1374cc4a072a93a3f1b2e3aa24f4
Oliver Berghold <info@oliverberghold.de>
2021-10-14 12:57:16 +0200

    create grunt task runner that compiles and watch sass files and create sass map / add sass files

---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * Project banner
+         */
+        tag: {
+            banner: '/*!\n' +
+                ' * <%= pkg.name %>\n' +
+                ' * <%= pkg.title %>\n' +
+                ' * <%= pkg.url %>\n' +
+                ' * @author <%= pkg.author %>\n' +
+                ' * @version <%= pkg.version %>\n' +
+                ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+                ' */\n'
+        },
+
+
+        sass: {
+            dist: {
+                files: {
+                    'css/boilerplate.css': 'scss/boilerplate.scss'
+                }
+            }
+        },
+
+
+        watch: {
+            options: {
+                livereload: true,
+                spawn: false
+            },
+            css: {
+                files: ['scss/**'],
+                tasks: ['sass']
+            }
+        },
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass', 'watch']);
+
+};
